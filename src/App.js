aa85b471d6019cf023a221729c8694dd3ce66622
Alexey  Nakhaev <anakhaev@dfusiontech.com>
2021-10-12 18:02:40 +0300

    Bootstrap & Reactstrap were added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import config from './config';
+import { Button, ButtonGroup, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Row, Col  } from 'reactstrap';
 
 function App() {
   const {apiUrl} = config;
+
+  const [dropdownOpen, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!dropdownOpen);
 
   return (
     <div className="App">
@@ -21,6 +27,45 @@ function App() {
           Learn React
         </a>
         <p>{apiUrl}</p>
+        <p>Bootstrap buttons</p>
+        <Row>
+          <Col>
+            <ButtonGroup>
+              <Button>Left</Button>
+              <Button>Middle</Button>
+              <Button>Right</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+              <DropdownToggle caret>
+                Button Dropdown
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem disabled>Action</DropdownItem>
+                <DropdownItem>Another Action</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Another Action</DropdownItem>
+              </DropdownMenu>
+            </ButtonDropdown>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>
+              <Button color="primary">primary</Button>{' '}
+              <Button color="secondary">secondary</Button>{' '}
+              <Button color="success">success</Button>{' '}
+              <Button color="info">info</Button>{' '}
+              <Button color="warning">warning</Button>{' '}
+              <Button color="danger">danger</Button>{' '}
+              <Button color="link">link</Button>
+            </div>
+          </Col>
+        </Row>
       </header>
     </div>
   );
